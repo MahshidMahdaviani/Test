8751b3a9db26c664f10c4410f41c255911f74078
Mahshid Mahdaviani <mahshid.mahdaviani@gmail.com>
2019-10-15 10:53:32 +0330

    version1

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Personnel> personnels = new List<Personnel>();
+            personnels.Add(new Personnel() { Name = "ali", PersonnelCode = 1 });
+            personnels.Add(new Personnel() { Name = "mahdi", PersonnelCode = 2 });
+            ViewData["msg"] = "This is a test";
+            ViewBag.message2 = "this is a data from viewbag";
+            ViewBag.Data = personnels;
+            return View(personnels);
+        }
+
+        public ActionResult FirstLayoutSample()
+        {
+            return View();
+        }
+
+        public ActionResult TestLayout()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/LearningController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LearningController : Controller
+    {
+        // GET: Learning
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Razer01()
+        {
+            return View();
+        }
+        public ActionResult Razer02()
+        {
+            return View();
+        }
+        public ActionResult LoadImage()
+        {
+            ViewBag.Item = 10;
+            return View();
+        }
+    }
+}
